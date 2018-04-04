8d1090a6074fa950c13853dfa4cb695653e19060
Amal Poulose <amalpoulose1995@hotmail.com>
2018-04-04 14:06:24 +0530

    Create 21.py

---
+++ 21.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python2.7
+
+def spchr_rm(l):
+    it=iter(l[:]) # to avoid shallow copy
+    try:
+        while True:
+    
+            temp=next(it)
+            if not temp.isalpha():
+                l.remove(temp)
+    except StopIteration:
+        pass
+    l.sort()
+
+def main():
+    s1=raw_input("string 1 : ")
+    s2=raw_input("string 2 : ")
+    l1=list(s1)
+    l2=list(s2)
+    spchr_rm(l1)
+    spchr_rm(l2)
+    if l1==l2:
+        print "Anagram"  
+    else:
+        print "not anagram"
+    
+if __name__=="__main__":
+    main()
