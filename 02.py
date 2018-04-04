4305bb3fc3662b85f638f98b8b41cd47bb06981a
Amal Poulose <amalpoulose1995@hotmail.com>
2018-04-04 10:38:50 +0530

    Update 02.py

--- 02.py
+++ 02.py
@@ -2,7 +2,7 @@
 
 def main():
     s1=raw_input("sting : ")
-    s2=''.join([i for i in s1]) //copy sting to another buffer
+    s2=''.join([i for i in s1]) #copy sting to another buffer
     print "\n s1 = ",s1
     print "s2 = ",s2
     
