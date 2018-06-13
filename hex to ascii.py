b41b03acaa5adc4b07779686f372fb4778cbc46c
Amal Poulose <amalpoulose1995@hotmail.com>
2018-06-13 08:25:21 +0530

    Update hex to ascii.py

--- hex to ascii.py
+++ hex to ascii.py
@@ -4,15 +4,18 @@ import math
 def main():
     p,temp=0,0
     string=''
-    l=raw_input().split()
+    l=raw_input().strip().split()
     for i in l:
         p=len(i)-1
         for j in i:
             if j.isdigit():
                 temp+=int(j)*16**p
             else:
-                assert j.upper()<="F"  # if you enter input as 4g it will raise exception 
-                temp+=(ord(j.upper())-55)*16**p            
+                try:
+                    assert j.upper()<="F"  # if you enter input as 4g it will raise exception 
+                    temp+=(ord(j.upper())-55)*16**p
+                except AssertionError:
+                    print "Base Error"  # Error message the value exceed  eg: input is 4g 4n
             p-=1
         string+=chr(temp)
         temp=0
@@ -20,5 +23,6 @@ def main():
 
 if __name__=="__main__":
     main()
+        
         
             
